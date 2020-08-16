7f774d794f0a68768fca634e24f61bd1547983ff
superstark02 <superstark02@gmail.com>
2020-08-16 10:53:13 +0530

    8/16/2020

---
+++ src/Pages/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../CSS/Home/Carousel.css'
+
+export class About extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default About
