2c78647df2d7eb034c9bc8bb01932ec1615f2d9f
superstark02 <superstark02@gmail.com>
2020-08-22 10:34:15 +0530

    8/22/2020

---
+++ src/Database/Constants/Projects.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import project1 from '../../Images/Projects/html.png'
+
+export const projects = [
+    {
+        name: "Project - 1",
+        des: "This some very long text",
+        date: "DD:MM:YYYY",
+        image: project1
+    },
+    {
+        name: "Project - 2",
+        des: "This some very long text description",
+        date: "DD:MM:YYYY",
+        image: project1
+    },
+    {
+        name: "Project - 3",
+        des: "This some very long text description",
+        date: "DD:MM:YYYY",
+        image: project1
+    },
+]
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react'
-import About from './About'
+import '../CSS/Pages/Home.css'
+import { projects } from '../Database/Constants/Projects'
 
 export class Home extends Component {
     render() {
         return (
             <div>
-                
+                <div className="home-bg wrap" >
+                    <div style={{ width: '70%' }} >
+                        DTU ALTAIR
+                    </div>
+                </div>
+
+                <div className="wrap" >
+                    <div className="page-wrap" >
+                        <h1>Content</h1>
+                        <div className="projects" >
+                            {
+                                projects.map(item => {
+                                    return (
+                                        <div className="projects-card" >
+                                            <div>
+                                                <img width="100px" src={item.image} />
+                                            </div>
+                                            <div>
+                                                <h2>{item.name}</h2>
+                                            </div>
+                                            <div>
+                                                {item.des}
+                                            </div>
+                                            <div>
+                                                {item.date}
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+
+                <div style={{height:"50vh"}} ></div>
+
             </div>
         )
     }
